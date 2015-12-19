bc3e8e242ab573081cd9250238d17bd5ea1d3667
Eugene Sukharev <eugenes@ecobee.com>
2015-12-18 20:11:19 -0500

    app message

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -7,3 +7,7 @@
 Pebble.addEventListener('ready', function(e) {
     console.log('JavaScript app ready and running!');
 });
+
+Pebble.addEventListener('appmessage', function(e) {
+    console.log('Received message: ' + JSON.stringify(e.payload));
+});
