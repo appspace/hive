45fcc31c584b09df03f8459264ef2f0c0c6cf8ea
Eugene <eugenes.mobile@gmail.com>
2015-12-22 14:07:32 -0500

    module extracted

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is the app run on pebble watches
- */
-
-var UI = require('ui');
-var Vector2 = require('vector2');
 var ajax = require('ajax');
 var Vibe = require('ui/vibe');
+var Settings = require('settings');
+var UI = require('ui');
+var Vector2 = require('vector2');
+
+Settings.data({
+  ecobeeServerUrl: 'https://api.ecobee.com', 
+  ecobeeTokenApi: '/token',
+  ecobeeApiEndpoint: '/1/analytics',
+  paired: false, 
+  refreshToken: null, 
+  oauthToken: null, 
+  oauthTokenExpires: null,
+  authPin: null,
+  authCode: null,
+  authExpires: null,
+  clientId: 'ABC123'
+});
 
 var mainWindow = new UI.Window();
 
@@ -28,24 +37,139 @@ var temperatureText = new UI.Text({
 
 mainWindow.add(background);
 mainWindow.add(temperatureText);
-mainWindow.show();
+mainWindow.setText = function(text) {
+  temperatureText.text(text);
+};
 
-Pebble.addEventListener('appmessage', function(e) {
-  console.log('Received message: ' + JSON.stringify(e.payload));
-});
+var authorizePin = function(pin, code) {
+  var card = new UI.Card();
+  card.title(pin);
+  card.body('Log into ecobee.com and click "Add Application" in "My Apps" section.' +
+      'Enter the provided pin then '+
+      'press select button to continue with authorization');
+  card.show();
+  card.on('click', function(e) {
+    var tokenUrl =  Settings.data('ecobeeServerUrl') +
+        Settings.data('ecobeeTokenApi') +
+        '?grant_type=ecobeePin&client_id=' +
+        Settings.data('clientId') +
+        "&code=" + code;
+    console.log('Calling '+tokenUrl);
+    ajax(
+        {
+          url: tokenUrl,
+          type: 'json',
+          method: 'post'
+        },
+        function(data) {
+          Vibe.vibrate('short');
+          console.log('Received AUTH data: '+JSON.stringify(data));
+          Settings.data('authPin', null);
+          Settings.data('authCode', null);
+          Settings.data('authExpires', null);
+          Settings.data('paired', true);
+          Settings.data('refreshToken', data.refresh_token);
+          Settings.data('oauthToken', data.access_token);
+          var tokenExpiresIn = Date.now()+data.expires_in*1000;
+          console.log('Token '+data.access_token+' will expire at '+tokenExpiresIn);
+          Settings.data('oauthTokenExpires', tokenExpiresIn);
+          initialCheck();
+        },
+        function(error) {
+          console.log('Error receiving AUTH data: '+JSON.stringify(error));
+          if (error.error === "authorization_pending") {
+            //card.hide();
+            authorizePin(pin, code);
+          } else {
+            card = new UI.Card();
+            card.title('Auth Error');
+            card.body(error.error_description);
+            card.show();
+          }
+        }
+    );
+  });
+  Settings.data('paired', true);
+}
+
+var getPin = function() {
+  Settings.data('authPin', null);
+  Settings.data('authCode', null);
+  Settings.data('authExpires', null);
+  var card = new UI.Card();
+  card.title('Authorization');
+  card.body('Calling ecobee for authorization. Please wait.');
+  card.show();
+  var authUrl = Settings.data('ecobeeServerUrl')
+      +'/authorize?response_type=ecobeePin&scope=smartWrite&client_id='
+      +Settings.data('clientId');
+  ajax(
+      {
+        url: authUrl,
+        type: 'json'
+      },
+      function(data) {
+        Vibe.vibrate('short');
+        console.log('Received AUTH data: '+JSON.stringify(data));
+        Settings.data('authPin', data.ecobeePin);
+        Settings.data('authCode', data.code);
+        var expiresAt = Date.now()+data.expires_in*60*1000;
+        console.log('Pin will expire at '+expiresAt);
+        Settings.data('authExpires', expiresAt);
+        //card.hide();
+        authorizePin(data.ecobeePin, data.code);
+      },
+      function(error) {
+        console.log('Error receiving AUTH data: '+JSON.stringify(error));
+        //card.hide();
+        card = new UI.Card();
+        card.title('Auth Error');
+        card.body('Unable to contact ecobee. Try again later.');
+        card.show();
+      }
+  );
+}
+
+var initialCheck = function() {
+  var isPaired = Settings.data('paired');
+  console.log('Already paired? '+isPaired);
+  if (isPaired) {
+    mainWindow.show();
+  } else {
+    var pinExpiration = Settings.data('authExpires');
+    console.log('Pin expiration: '+pinExpiration);
+    if (!pinExpiration) {
+      getPin();
+    } else if (Date.now() > pinExpiration-5000) {
+      getPin();
+    } else {
+      var pin = Settings.data('authPin');
+      var code = Settings.data('authCode');
+      authorizePin(pin,  code);
+    }
+  }
+}
+
+initialCheck();
 
 mainWindow.on('click', 'select', function(event) {
   console.log('Click event on mid button');
-  temperatureText.text('Loading...');
-  ajax({ url: 'https://api.ecobee.com/1/analytics', type: 'json' },
+  mainWindow.setText('Loading...');
+  var callUrl = Settings.data('ecobeeServerUrl')+Settings.data('ecobeeApiEndpoint');
+  console.log('Calling '+callUrl);
+  ajax(
+      { 
+        url: callUrl, 
+        type: 'json' 
+      },
       function(data) {
         console.log('Received data.');
         Vibe.vibrate('short');
-        temperatureText.text('Data OK');
+        mainWindow.setText('Data OK');
       },
       function(error) {
-        console.log('Error receiving ecobee data.');
-        temperatureText.text('Error');
+        console.log('Error receiving ecobee data: '+JSON.stringify(error));
+        mainWindow.setText('Error');
       }
   );
 
