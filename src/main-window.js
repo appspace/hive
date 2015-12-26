be3d3ca66bcb821fd00f2b3cf69dcdfbabadc7f0
Eugene <eugenes.mobile@gmail.com>
2015-12-26 11:39:26 -0500

    tstat mode

--- src/main-window.js
+++ src/main-window.js
@@ -1,51 +1,50 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
-var Settings = require('settings');
 var ecobeeApi = require('ecobee-api');
 var ErrorWindow = require('error-window');
 
-var mainWindow;
-
-if (Pebble.getActiveWatchInfo().platform==='aplite') {
-//if (true) {  //Uncomment to test aplite on pebble color
-  mainWindow = new UI.Window({
-    action: {
-      up: 'images/up-bw.png',
-      select: 'images/menu-bw.png',
-      down: 'images/down-bw.png', 
-      backgroundColor: 'white'
-    }
-  });  
-} else {
-  mainWindow = new UI.Window({
-    action: {
-      up: 'images/up-color.png',
-      select: 'images/menu-color.png',
-      down: 'images/down-color.png', 
-      backgroundColor: 'white'
-    }
-  });  
-}
+var mainWindow = new UI.Window({
+  fullscreen: true, 
+  scrollable: false, 
+  clear: false
+});
 
 var nameText = new UI.Text({
   position: new Vector2(0, 0),
-  size: new Vector2(120, 20),
+  size: new Vector2(144, 20),
   text: 'Loading...',
   font: 'gothic-18-bold',
   color: 'white',
   textAlign: 'left'
 });
 
-var temperatureText = new UI.Text({
+var humidityText = new UI.Text({
   position: new Vector2(0, 40),
-  size: new Vector2(120, 40),
+  size: new Vector2(144, 20),
+  text: '',
+  font: 'gothic-18',
+  color: 'white',
+  textAlign: 'center'
+});
+
+var temperatureText = new UI.Text({
+  position: new Vector2(0, 60),
+  size: new Vector2(144, 40),
   text: '...',
   font: 'bitham-42-bold',
   color: 'white',
   textAlign: 'center'
 });
 
+var modeImage = new UI.Image({
+  position: new Vector2(0, 126),
+  size: new Vector2(18, 168),
+  image: 'img/bg-heat.png'
+});
+
+mainWindow.add(modeImage);
 mainWindow.add(nameText);
+mainWindow.add(humidityText);
 mainWindow.add(temperatureText);
 mainWindow.setTstatName = function(text) {
   nameText.text(text);  
@@ -53,6 +52,9 @@ mainWindow.setTstatName = function(text) {
 mainWindow.setTemperature = function(canonical) {
   var celsius = (canonical/10-32)*5/9;
   temperatureText.text(celsius.toPrecision(3));
+};
+mainWindow.setHumidity = function(percentage) {
+  humidityText.text(percentage+'%');
 };
 
 //mainWindow.on('click', 'select', function(event) {
@@ -64,6 +66,7 @@ mainWindow.on('show', function(event) {
             var name = thermostat.name;
             mainWindow.setTstatName(name);
             mainWindow.setTemperature(thermostat.runtime.actualTemperature);
+            mainWindow.setHumidity(thermostat.runtime.actualHumidity);
       }, 
       function(error) {
             ErrorWindow.show('Cannot load thermostat data');
